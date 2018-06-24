75834a2d27f8c6a2752095447c8a5ff81045dc5b
KMouratidis <k.mouratidis@Hotmail.com>
2018-06-24 21:12:22 +0300

    Added a setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="google_dl",
+    version="1.0",
+    author="mycognitive",
+    description="Download files from google search.",
+    url="https://github.com/mycognitive/google_dl",
+    packages=setuptools.find_packages(),
+    classifiers=("Programming Language :: Python :: 3",)
+)
